5b86f4863863452668d0a76bc31efd5c0d0f989b
vkaszuba <codeword.vika@gmail.com>
2020-01-17 16:19:00 -0800

    Code quiz title now turns into the timer

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
-
-
-const startQuizBtn = document.querySelector("#startQuiz");
-startQuizBtn.addEventListener("click", setTime);
+var time = document.querySelector(".time");
 
 var secondsLeft = 5;
 
+// Start Quiz button, on click runs setTime function
+const startQuizBtn = document.querySelector("#startQuiz");
+startQuizBtn.addEventListener("click", setTime);
+
+// Starts time countdown
 function setTime() {
+    
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    time.textContent = `Time Remaining: ${secondsLeft} seconds`;
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      timeEl.textContent = "Time has ended";
+      time.textContent = "Time has ended";
     }
 
   }, 1000);
