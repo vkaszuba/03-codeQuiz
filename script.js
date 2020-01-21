ea8cfba94cab9cc4644379da8332af4a6d33e6de
vkaszuba <codeword.vika@gmail.com>
2020-01-20 19:48:56 -0800

    Structure of quiz set up, basics are functioning

--- script.js
+++ script.js
@@ -1,76 +1,58 @@
-const timer = document.querySelector(".timer");
-
+const timer = document.querySelector("#timer");
+const quizEl = document.querySelector('#quiz');
 let secondsLeft = 5;
-let showResults = "";
-let userChoice = "";
-let currentQuestion = "";
-
-
+let currentQuestion = 0;
+let score = 0;
 
 
 // Start Quiz button, on click runs setTime function
 const startQuizBtn = document.querySelector("#startQuiz");
-startQuizBtn.addEventListener("click", setTime, quizStart);
-
+startQuizBtn.addEventListener("click", setTime );
+startQuizBtn.addEventListener("click", quizStart);
 // Starts time countdown
 function setTime() {
 
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timer.textContent = `Time Remaining: ${secondsLeft} seconds`;
-
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
       clearInterval(timerInterval);
       timer.textContent = "Time has ended";
       // Add display of score here
     }
-
   }, 1000);
 };
-
-
-
-
-
 function quizStart() {
 
+  displayQuestion();
 };
+function displayQuestion() {
 
-function choiceA() {
-  return questions[0].choices[0];
+  let questList = questions[currentQuestion].choices.map((question)=>{
+    return `<button class="answerBtn" onclick="answerQuestion('${question}')">${question}</button>`;
+  })
+
+  quiz.innerHTML = `${questions[currentQuestion].title}<br>${questList.join("")}`;
 }
 
 
+function answerQuestion(selection) {
+  if (questions[currentQuestion].answer === selection) {
+    score ++;
+    secondsLeft += 5;
+  } else {
+    secondsLeft -= 5;
+  }
+  currentQuestion ++;
+
+  if (currentQuestion === questions.length) {
+    endGame();
+    return;
+  }
+  displayQuestion();
+}
 
 
-const quizQuestion = document.querySelector("#quizQuestion");
-const optionA = document.querySelector("#optionA");
-const optionB = document.querySelector("#optionB");
-const optionC = document.querySelector("#optionC");
-const optionD = document.querySelector("#optionD");
-
-// optionA.addEventListener("click", choiceA);
-// optionB.addEventListener("click", choiceB);
-// optionC.addEventListener("click", choiceC);
-// optionD.addEventListener("click", choiceD);
-
-// Maybe set to questions[i] so it can be changed per new button click
-quizQuestion.textContent = questions[0].title;
-optionA.textContent = questions[0].choices[0];
-optionB.textContent = questions[0].choices[1];
-optionC.textContent = questions[0].choices[2];
-optionD.textContent = questions[0].choices[3];
-
-
-
-// Adding time to correct answer, subtract for wrong
-// Change first part to reflect user choice
-if (userChoice === questions[currentQuestion].answer) {
-  secondsLeft = secondsLeft + 5;
-  console.log("Correct");
-
-} else {
-  secondsLeft = secondsLeft - 5;
-  console.log("Incorrect");
-
+function endGame() {
+  quiz.innerHTML = `you got ${score} of ${questions.length}`;
 }
