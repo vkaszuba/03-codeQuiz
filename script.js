2d7bdb4b1193e3ed399904225ba1e851b873e007
vkaszuba <codeword.vika@gmail.com>
2020-01-20 17:29:23 -0800

    Fixed indentation issue, Questions JS properly linked now

--- script.js
+++ script.js
@@ -4,20 +4,6 @@ let secondsLeft = 5;
 let showResults = "";
 
 
-const questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    ///etc.
-  ];
-
 
 
 // Start Quiz button, on click runs setTime function
@@ -26,12 +12,12 @@ startQuizBtn.addEventListener("click", setTime, quizStart);
 
 // Starts time countdown
 function setTime() {
-    
-  var timerInterval = setInterval(function() {
+
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     time.textContent = `Time Remaining: ${secondsLeft} seconds`;
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
       time.textContent = "Time has ended";
       // Add display of score here
@@ -48,10 +34,11 @@ function quizStart() {
 
 
 
-let choiceA = questions[1][0];
-let choiceB = questions[1][1];
-let choiceC = questions[1][2];
-let choiceD = questions[1][3];
+function choiceA() {
+
+
+  return questions[0].choices[0];
+}
 
 const quizQuestion = document.querySelector("#quizQuestion");
 const optionA = document.querySelector("#optionA");
@@ -59,28 +46,28 @@ const optionB = document.querySelector("#optionB");
 const optionC = document.querySelector("#optionC");
 const optionD = document.querySelector("#optionD");
 
-optionA.addEventListener("click", choiceA);
-optionB.addEventListener("click", choiceB);
-optionC.addEventListener("click", choiceC);
-optionD.addEventListener("click", choiceD);
+// optionA.addEventListener("click", choiceA);
+// optionB.addEventListener("click", choiceB);
+// optionC.addEventListener("click", choiceC);
+// optionD.addEventListener("click", choiceD);
 
-
+// Maybe set to questions[i] so it can be changed per new button click
 quizQuestion.textContent = questions[0].title;
 optionA.textContent = questions[0].choices[0];
 optionB.textContent = questions[0].choices[1];
 optionC.textContent = questions[0].choices[2];
 optionD.textContent = questions[0].choices[3];
 
+
+
 // Adding time to correct answer, subtract for wrong
+// Change first part to reflect user choice
 if (questions[0].choices[2] === questions[0].answer) {
-    secondsLeft = secondsLeft + 5;
+  secondsLeft = secondsLeft + 5;
+  console.log("Correct");
+
 } else {
-    secondsLeft = secondsLeft - 5;
+  secondsLeft = secondsLeft - 5;
+  console.log("Incorrect");
+
 }
-
-
-console.log(questions[0].choices);
-console.log(questions[1].choices);
-
-console.log(questions[0].answer);
-console.log(questions[0].choices[2]);
