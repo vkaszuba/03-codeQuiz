f2d02294bbeea6ad40c915df6565349d4a3e7387
vkaszuba <codeword.vika@gmail.com>
2020-01-23 21:29:51 -0800

    Adding questions to quiz

--- questions.js
+++ questions.js
@@ -1,18 +1,32 @@
 const questions = [
   {
-    title: "Which Hogwarts class is Harry in?",
+    title: "Which Hogwarts house is Harry in?",
     choices: ["Slytherin", "Gryffindor", "Hufflepuff", "Ravenclaw"],
     answer: "Gryffindor"
   },
   {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    title: "Who is the broomstick professor?",
+    choices: ["Hooch", "McGonagall", "Dumbledore", "Finch"],
+    answer: "Hooch"
   },
   {
-    title: "Harry Potter's best friend's name:",
-    choices: ["Larry", "Dean", "Draco", "Ron"],
+    title: "Harry Potter's best friends name:",
+    choices: ["James", "Dean", "Draco", "Ron"],
     answer: "Ron"
   },
-  
+  {
+    title: "What type of creature is Fluffy?",
+    choices: ["Dragon", "Cat", "Three-Headed Dog", "Owl"],
+    answer: "Three-Headed Dog"
+  },
+  {
+    title: "Who killed Dumbledore?",
+    choices: ["Draco", "Bellatrix", "Voldemort", "Snape"],
+    answer: "Snape"
+  },
+  {
+    title: "Who is Bill's daughter??",
+    choices: ["Hermione", "Luna", "Victoire", "Minerva"],
+    answer: "Victoire"
+  },
 ];
